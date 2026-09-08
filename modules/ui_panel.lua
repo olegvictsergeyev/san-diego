9a3b01c4cab19db3ae019c8569b0cb7772836135
O.V.Sergeyev <oleg.vict.sergeyev@gmail.com>
2026-09-08 21:00:54 +0300

    откат лимита комнаты обратно до 50 (20 был следствием фейлов постановки на мобиле, не серверным лимитом); pickup: пауза репликации 0.7->1.1с; buy: confirm timeout 3->5с под мобильную репликацию (v2.12.17)

--- modules/ui_panel.lua
+++ modules/ui_panel.lua
@@ -9,7 +9,7 @@
 
 local CONFIG = {
     -- Версия агента (major.minor.patch). Сейчас ранняя альфа.
-	version = "2.12.16",
+	version = "2.12.17",
 
     -- URL существующего сервиса
     baseUrl = "http://195.161.68.193:5173/api",
